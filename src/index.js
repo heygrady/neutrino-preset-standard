2da089b01d6e942c89e0bf672775a4f9021f5ceb
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-03-28 16:20:20 +0200

    Support Neutrino 5.0

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
-const lint = require('neutrino-lint-base');
+const lint = require('neutrino-middleware-eslint');
 const merge = require('deepmerge');
 
-module.exports = neutrino => {
-  lint(neutrino);
-  neutrino.config.module
-    .rule('lint')
-    .loader('eslint', props => merge(props, {
-      options: {
-        baseConfig: {
-          extends: ['standard']
-        },
-        rules: {
-          // handled by babel rules
-          'new-cap': 'off',
+module.exports = (neutrino, options) => {
+  neutrino.use(lint, merge({
+    eslint: {
+      baseConfig: {
+        extends: ['standard']
+      },
+      rules: {
+        // handled by babel rules
+        'new-cap': 'off',
 
-          // handled by babel rules
-          'object-curly-spacing': 'off',
+        // handled by babel rules
+        'object-curly-spacing': 'off',
 
-          // require a capital letter for constructors
-          'babel/new-cap': ['error', { newIsCap: true }],
+        // require a capital letter for constructors
+        'babel/new-cap': ['error', { newIsCap: true }],
 
-          // require padding inside curly braces
-          'babel/object-curly-spacing': ['error', 'always'],
+        // require padding inside curly braces
+        'babel/object-curly-spacing': ['error', 'always'],
 
-          // guard against awaiting async functions inside of a loop
-          'babel/no-await-in-loop': 'error'
-        }
+        // guard against awaiting async functions inside of a loop
+        'no-await-in-loop': 'error'
       }
-    }));
+    }
+  }, options));
+  if (!options.include && !options.exclude) {
+    neutrino.config.module
+      .rule('lint')
+      .include
+      .add(neutrino.options.source);
+  }
 };
