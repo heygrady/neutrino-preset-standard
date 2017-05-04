a75aa013db7b8ef5d0b3906e73415f6832508e48
Grady Kuhnline <github@heygrady.net>
2017-05-04 00:35:02 -0700

    bumping to 5.8.0

---
+++ index.js
@@ -0,0 +1,32 @@
+const lint = require('neutrino-middleware-eslint');
+const merge = require('deepmerge');
+
+module.exports = (neutrino, options) => {
+  neutrino.use(lint, merge({
+    eslint: {
+      baseConfig: {
+        extends: ['standard']
+      },
+      rules: {
+        // handled by babel rules
+        'new-cap': 'off',
+
+        // handled by babel rules
+        'object-curly-spacing': 'off',
+
+        // require a capital letter for constructors
+        'babel/new-cap': ['error', { newIsCap: true }],
+
+        // require padding inside curly braces
+        'babel/object-curly-spacing': ['error', 'always'],
+
+        // guard against awaiting async functions inside of a loop
+        'no-await-in-loop': 'error'
+      }
+    }
+  }, options));
+
+  neutrino.config.module.rule('lint')
+    .when(!options.include && !options.exclude,
+      rule => rule.include.add(neutrino.options.source));
+};
